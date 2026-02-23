02c8849a5b930e8eeb5db433d430c2df4903b6ee
kyungboshim <kyungbo.shim@neowiz.com>
2026-02-23 13:19:24 +0900

    5.2.0 배포

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AdiscopeMediaTnkPub",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.2/AdiscopeMediaTnkPub.zip",
-            checksum: "d7e90bd5ca8d958f5e2429cd2bcc8634437e66d2e26f4287c337f078eaf7ae4b"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.0/AdiscopeMediaTnkPub.zip",
+            checksum: "63bdb060d1aa7c4d17aab5241ada6394ee9ecb925cc94c6fac00742483e5d374"
         ),
         .binaryTarget(
             name: "TnkPubSdk",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.2/TnkPubSdk.xcframework.zip",
-            checksum: "d7e90bd5ca8d958f5e2429cd2bcc8634437e66d2e26f4287c337f078eaf7ae4b"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.0/TnkPubSdk.xcframework.zip",
+            checksum: "d96cccf26eff04f2b3df845124638f3facf7703b298abaac586a4ff636a5040c"
         ),
     ]
 )
